837f25294947259a4e830af74c1a89a3a5cc7adc
Filip <fplavac1@gmail.com>
2023-10-18 21:31:55 +0200

    master - made all properties of Title model nullable

--- Models/Title.cs
+++ Models/Title.cs
@@ -3,13 +3,13 @@
     public class Title
     {
         public required string Id { get; set; }
-        public int ContentTypeId { get; set; }
-        public string PrimaryTitle { get; set; }
-        public string OriginalTitle { get; set; }
-        public int IsAdult { get; set; }
-        public int StartYear { get; set; }
-        public int EndYear { get; set; }
-        public int RuntimeMinutes { get; set; }
+        public int? ContentTypeId { get; set; }
+        public string? PrimaryTitle { get; set; }
+        public string? OriginalTitle { get; set; }
+        public int? IsAdult { get; set; }
+        public int? StartYear { get; set; }
+        public int? EndYear { get; set; }
+        public int? RuntimeMinutes { get; set; }
 
     }
 }
