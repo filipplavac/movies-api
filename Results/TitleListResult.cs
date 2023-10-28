4b9f1c58799becc32a83a5a0697c314654ceceb6
Filip <fplavac1@gmail.com>
2023-10-28 19:25:41 +0200

    master - created TitleListResult class

---
+++ Results/TitleListResult.cs
@@ -0,0 +1,15 @@
+﻿using movies_api.Models;
+
+namespace movies_api.Results
+{
+    public class TitleListResult
+    {
+        public string? Cursor { get; }
+        public List<Title>? Titles { get; }
+        public TitleListResult(List<Title> titles, string cursor) 
+        { 
+            Cursor = cursor;
+            Titles = titles;
+        }
+    }
+}
