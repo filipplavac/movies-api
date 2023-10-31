3008455d4f41b3ed7426a4b4d57265281c7b3335
Filip <fplavac1@gmail.com>
2023-10-31 17:07:59 +0100

    master - added ToDto and FromRecord methods to Title model

--- Models/Title.cs
+++ Models/Title.cs
@@ -1,6 +1,9 @@
-﻿namespace movies_api.Models
+﻿using movies_api.Contracts.Dtos;
+using Npgsql;
+
+namespace movies_api.Models
 {
-    public class Title
+    public class Title 
     {
         public required string Id { get; set; }
         public int? ContentTypeId { get; set; }
@@ -11,5 +14,35 @@
         public int? EndYear { get; set; }
         public int? RuntimeMinutes { get; set; }
 
+        public static Title FromRecord(NpgsqlDataReader data)
+        {
+            Title title = new Title
+            {
+                Id = data["title_id"].ToString(),
+                ContentTypeId = data["content_type_id"].Equals(DBNull.Value) ? null : Convert.ToInt32(data["content_type_id"]),
+                PrimaryTitle = data["primary_title"].Equals(DBNull.Value) ? null : data["primary_title"].ToString(),
+                OriginalTitle = data["original_title"].Equals(DBNull.Value) ? null : data["original_title"].ToString(),
+                IsAdult = data["is_adult"].Equals(DBNull.Value) ? null : Convert.ToInt32(data["is_adult"]),
+                StartYear = data["start_year"].Equals(DBNull.Value) ? null : Convert.ToInt32(data["start_year"]),
+                EndYear = data["end_year"].Equals(DBNull.Value) ? null : Convert.ToInt32(data["end_year"]),
+                RuntimeMinutes = data["runtime_minutes"].Equals(DBNull.Value) ? null : Convert.ToInt32(data["runtime_minutes"])
+            };
+            return title;
+        }
+
+        public TitleDto ToDto()
+        {
+            return new TitleDto 
+                (
+                    Id,
+                    ContentTypeId,
+                    PrimaryTitle,
+                    OriginalTitle,
+                    IsAdult,
+                    StartYear,
+                    EndYear,
+                    RuntimeMinutes
+                );
+        }
     }
 }
