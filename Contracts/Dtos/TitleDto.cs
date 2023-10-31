429b71b1611d5c03dbb48d39edfa91b5bb76d12f
Filip <fplavac1@gmail.com>
2023-10-31 17:05:15 +0100

    master - created TitleDto class

---
+++ Contracts/Dtos/TitleDto.cs
@@ -0,0 +1,14 @@
+﻿namespace movies_api.Contracts.Dtos
+{
+    public record TitleDto
+    (
+        string Id,
+        int? ContentTypeId,
+        string? PrimaryTitle,
+        string? OriginalTitle,
+        int? IsAdult,
+        int? StartYear,
+        int? EndYear,
+        int? RuntimeMinutes
+    );
+}
