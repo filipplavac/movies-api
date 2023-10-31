3f9c940fe4a6bf8212d48ab3aa311891b495a861
Filip <fplavac1@gmail.com>
2023-10-31 17:07:05 +0100

    master - moved Results folder under Contracts and modified The TitleListResult class Titles member

---
+++ Contracts/Results/TitleListResult.cs
@@ -0,0 +1,15 @@
+﻿using movies_api.Contracts.Dtos;
+
+namespace movies_api.Contracts.Results
+{
+    public class TitleListResult
+    {
+        public string? Cursor { get; }
+        public List<TitleDto>? Titles { get; }
+        public TitleListResult(List<TitleDto>? titles, string? cursor) 
+        { 
+            Cursor = cursor;
+            Titles = titles;
+        }
+    }
+}
