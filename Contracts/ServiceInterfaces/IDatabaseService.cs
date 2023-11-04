fc2dc9f911ab7e80ab63609e8ddd343b52c74d32
Filip <fplavac1@gmail.com>
2023-11-04 20:39:36 +0100

    master - changed IDatabaseService interface to be generic

--- Contracts/ServiceInterfaces/IDatabaseService.cs
+++ Contracts/ServiceInterfaces/IDatabaseService.cs
@@ -1,7 +1,13 @@
 ﻿namespace movies_api.Contracts.ServiceInterfaces
 {
-    public interface IDatabaseService
+    public interface IDatabaseService<T>
     {
         public string ConnectionString { get; }
+
+        public Task<List<T>> GetList(string cursor, int pageSize);
+        //public T Get(string id);
+        //public T Create(T entity);
+        //public void Update(T entity);
+        //public void Delete(string id);
     }
 }
