eb8cd0ae77f27365b07c002aad86e122b7221b37
Filip <fplavac1@gmail.com>
2023-11-22 21:45:46 +0100

    get-title-list-filter - removed QueryToFilter method from IModelMapper interface

--- Contracts/ServiceInterfaces/IModelMapper.cs
+++ Contracts/ServiceInterfaces/IModelMapper.cs
@@ -2,11 +2,10 @@
 
 namespace movies_api.Contracts.ServiceInterfaces
 {
-    public interface IModelMapper<TModel, TDto, TFilter> where TModel : class where TDto : class
+    public interface IModelMapper<TModel, TDto> where TModel : class where TDto : class
     {
         TModel RecordToModel(NpgsqlDataReader data);
         TDto ModelToDto(TModel model);
         TModel DtoToModel(TDto dto);
-        TFilter QueryToFilter(string query);
     }
 }
